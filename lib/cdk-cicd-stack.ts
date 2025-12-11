c0390cd85dc3dbcbe5e88719fe5c0c7632c275a6
rajatgetwings <rajatgetwings@gmail.com>
2025-12-11 23:36:22 +0530

    new change

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,12 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import { PipelineStage } from './PipelineStage';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'CICDPipeline', {
+    const pipeline = new CodePipeline(this, 'CICDPipeline', {
       pipelineName: 'CICDPipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('ZillionClouds/aws-pipeline', 'cicd-practice'),
@@ -15,6 +16,10 @@ export class CdkCicdStack extends cdk.Stack {
           'npx cdk synth'
         ],
       })
-    })
+    });
+
+    const testStage = pipeline.addStage(new PipelineStage(this, 'PipelineTestStage', {
+      stageName: 'test-stage'
+    }));
   }
 }
